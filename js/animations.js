73b9fa8de1a2a96a29ce49b936d47f85f2301ba5
proden13 <proden2@yandex.ru>
2023-03-15 16:12:52 +0300

    Changes to be committed: 	modified:   index.html 	modified:   js/animations.js

--- js/animations.js
+++ js/animations.js
@@ -22,3 +22,25 @@ function increaseNumberAnimationStep(i, element, endNumber) {
   }
 
 initIncreaseNumberAnimation();
+
+document.querySelector("#budget").addEventListener("change", function
+nandleSelectChange(event) {
+  if (event.target.value === "other") {
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("form__group");
+    formContainer.classList.add("form__other-input");
+
+    const input = document.createElement("input");
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+
+    formContainer.appendChild(input);
+    document.querySelector("#form form").insertBefore(formContainer, document.querySelector(".form__submit"));
+  }
+
+  const otherInput = document.querySelector(".form__other-input");
+  if (event.target.value !== "other" && Boolean(otherInput)) {
+    const otherInput = document.querySelector(".form__other-input");
+    document.querySelector("#form form").removeChild(otherInput);
+  }
+});
